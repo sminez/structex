ea3e1a2c3778a7750b12ebc8ff3f85d88abb403f
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-11-02 21:06:53 +0000

    Add an explanation to the templating example

--- examples/templated_printing.rs
+++ examples/templated_printing.rs
@@ -5,6 +5,19 @@ use std::collections::HashMap;
 use structex::{Structex, StructexBuilder};
 use tinytemplate::TinyTemplate;
 
+// This is a much more complicated, compound expression. It is searching for Rust "impl" blocks
+// within a file, filtering out those that look like a trait implementation ("impl X for Y") before
+// running a parallel group that breaks the impl block into non-overlapping sections:
+//   - First the name of the type that owns the impl block is printed
+//   - Then, a second extract is run to locate all method signatures (up to the opening '{')
+//   - For each signature, a second nested parallel group is run:
+//     - If the signature includes "->" we assume it has a return type and we attempt to narrow the
+//       matching substring to locate both the method name and return type
+//     - If the signature doesn't include "->", we only narrow and look for the function name.
+//   - Then, for each signature we run one final parallel group that checks to see if "self" is
+//     mutable and uses all of that information to print out a summary for the method.
+//     - The template strings used for each branch are slight modifications of one another
+//       depending on which branch has been taken.
 const SE: &str = r#"
 x/^impl(?:<.*?>)?.*? (\w+)(?:.|\n)*?^\}/
 v/^impl(?:<.*?>)?.*? for/ {
@@ -12,12 +25,12 @@ v/^impl(?:<.*?>)?.*? for/ {
   p/\nimpl {1}/;
 
   x/fn(?:.|\n)*?\{/ {
-    g/->/ x/fn (\w+)(?:.|\n)*?-> (.*?)\w*\{/ {
+    g/->/ n/fn (\w+)(?:.|\n)*?-> (.*?)\w*\{/ {
       g/(&?('. ))?mut self/ p/  mut {1} -> {2}/;
       v/(&?('. ))?mut self/ p/      {1} -> {2}/;
     };
 
-    v/->/ x/fn (\w+)(?:.|\n)*\{/ {
+    v/->/ n/fn (\w+)(?:.|\n)*\{/ {
       g/(&?('. ))?mut self/ p/  mut {1} -> ()/;
       v/(&?('. ))?mut self/ p/      {1} -> ()/;
     };
