1a273f26a25bf77dc583e79cd3c99319167c03f3
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-11-03 19:39:35 +0000

    Add initial tagged match tests

--- tests/suite.rs
+++ tests/suite.rs
@@ -3,25 +3,48 @@ use simple_test_case::dir_cases;
 use simple_txtar::Archive;
 use structex::Structex;
 
-#[dir_cases("tests/data/basic")]
-#[test]
-fn basic(_path: &str, content: &str) {
-    let arr = Archive::from(content);
-    let se = &arr.get("se").as_ref().unwrap().content;
+fn parse_case<T>(arr: &Archive, f: impl Fn(String) -> T) -> (&str, &str, Vec<T>) {
+    let se = &arr.get("se").unwrap().content;
     let haystack = &arr.get("haystack").as_ref().unwrap().content;
-    let expected_matches: Vec<String> = arr
+    let expected_matches: Vec<T> = arr
         .get("matches")
         .unwrap()
         .content
         .trim_end()
         .lines()
         .map(|s| s.replace("\\n", "\n"))
+        .map(f)
         .collect();
 
+    (se, haystack, expected_matches)
+}
+
+#[dir_cases("tests/data/basic")]
+#[test]
+fn basic(_path: &str, content: &str) {
+    let arr = Archive::from(content);
+    let (se, haystack, expected_matches) = parse_case(&arr, |s| s);
     let se: Structex<Regex> = Structex::new(se).unwrap();
     let matches: Vec<String> = se
         .iter_tagged_captures(haystack)
         .map(|m| m.as_str().to_string())
+        .collect();
+
+    assert_eq!(matches, expected_matches);
+}
+
+#[dir_cases("tests/data/tagged_matches")]
+#[test]
+fn tagged_matches(_path: &str, content: &str) {
+    let arr = Archive::from(content);
+    let (se, haystack, expected_matches) = parse_case(&arr, |s| {
+        let (tag, s) = s.split_once(' ').unwrap();
+        (tag.chars().next().unwrap(), s.to_string())
+    });
+    let se: Structex<Regex> = Structex::new(se).unwrap();
+    let matches: Vec<(char, String)> = se
+        .iter_tagged_captures(haystack)
+        .map(|m| (m.tag().unwrap(), m.as_str().to_string()))
         .collect();
 
     assert_eq!(matches, expected_matches);
