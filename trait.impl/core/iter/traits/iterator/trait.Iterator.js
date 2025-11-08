d78aa397175ba11ab32536ffbaf760a55346d2a9
sminez <sminez@users.noreply.github.com>
2025-11-08 12:49:13 +0000

    deploy: e9b34bcc7043eda93c01bba453cd6cf01f105ee9

--- trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["structex",[["impl&lt;'s, 'h, R, H&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"structex/struct.TaggedCapturesIter.html\" title=\"struct structex::TaggedCapturesIter\">TaggedCapturesIter</a>&lt;'s, 'h, R, H&gt;<div class=\"where\">where\n    R: <a class=\"trait\" href=\"structex/re/trait.Re.html\" title=\"trait structex::re::Re\">Re</a>&lt;Haystack&lt;'h&gt; = H&gt;,\n    H: <a class=\"trait\" href=\"structex/re/trait.Haystack.html\" title=\"trait structex::re::Haystack\">Haystack</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["structex",[["impl&lt;'s, 'h, R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"structex/struct.TaggedCapturesIter.html\" title=\"struct structex::TaggedCapturesIter\">TaggedCapturesIter</a>&lt;'s, 'h, R&gt;<div class=\"where\">where\n    R: <a class=\"trait\" href=\"structex/re/trait.Re.html\" title=\"trait structex::re::Re\">Re</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[666]}
+//{"start":57,"fragment_lengths":[509]}
