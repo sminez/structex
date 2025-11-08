3fbf3ff00ee0e32fb4a3224e7cce2a6ede11e925
sminez <sminez@users.noreply.github.com>
2025-11-08 11:09:17 +0000

    deploy: 6170d36cd185185d90a5e9573196535964780f4c

--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["structex",[["impl&lt;'h, R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"structex/struct.TaggedCaptures.html\" title=\"struct structex::TaggedCaptures\">TaggedCaptures</a>&lt;'h, R&gt;<div class=\"where\">where\n    R: <a class=\"trait\" href=\"structex/re/trait.Re.html\" title=\"trait structex::re::Re\">Re</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["structex",[["impl&lt;H&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"structex/struct.TaggedCaptures.html\" title=\"struct structex::TaggedCaptures\">TaggedCaptures</a>&lt;H&gt;<div class=\"where\">where\n    H: <a class=\"trait\" href=\"structex/re/trait.Haystack.html\" title=\"trait structex::re::Haystack\">Haystack</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[457]}
+//{"start":57,"fragment_lengths":[467]}
