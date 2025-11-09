2f3f6a9c9c4ccfdde5a43b47d05bca625da6f9e9
sminez <sminez@users.noreply.github.com>
2025-11-09 21:56:33 +0000

    deploy: 51549879c07b5c75f3fbc80aa41fef23bf1cdd7d

--- structex/template/sidebar-items.js
+++ structex/template/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ErrorKind"],"struct":["Template"],"type":["Error"]};
+window.SIDEBAR_ITEMS = {"enum":["ErrorKind"],"struct":["Template"],"trait":["Context"],"type":["Error"]};
