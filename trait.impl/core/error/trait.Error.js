b0f0608843e6877f98831442b6cbaeb0690f2545
sminez <sminez@users.noreply.github.com>
2025-11-16 14:42:21 +0000

    deploy: 12f2ca90d4b78eaf39b3e0f1923966955d53473d

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["structex",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"structex/enum.Error.html\" title=\"enum structex::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"structex/template/enum.RenderError.html\" title=\"enum structex::template::RenderError\">RenderError</a>"]]]]);
+    var implementors = Object.fromEntries([["structex",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"structex/enum.Error.html\" title=\"enum structex::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"structex/template/enum.RenderError.html\" title=\"enum structex::template::RenderError\">RenderError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["structex",[["impl&lt;H&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"structex/struct.TaggedCaptures.html\" title=\"struct structex::TaggedCaptures\">TaggedCaptures</a>&lt;H&gt;<div class=\"where\">where\n    H: <a class=\"trait\" href=\"structex/re/trait.Sliceable.html\" title=\"trait structex::re::Sliceable\">Sliceable</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["structex",[["impl&lt;H&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"structex/struct.TaggedCaptures.html\" title=\"struct structex::TaggedCaptures\">TaggedCaptures</a>&lt;H&gt;<div class=\"where\">where\n    H: <a class=\"trait\" href=\"structex/re/trait.Sliceable.html\" title=\"trait structex::re::Sliceable\">Sliceable</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
