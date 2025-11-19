8aa64e8d9de325d9af2c0dd816c9536f230ef545
sminez <sminez@users.noreply.github.com>
2025-11-19 14:35:34 +0000

    deploy: 343e8aaf99d87503e3b32371db2ae87b49ea31bd

--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["structex",[["impl&lt;H&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"structex/struct.TaggedCaptures.html\" title=\"struct structex::TaggedCaptures\">TaggedCaptures</a>&lt;H&gt;<div class=\"where\">where\n    H: <a class=\"trait\" href=\"structex/re/trait.Sliceable.html\" title=\"trait structex::re::Sliceable\">Sliceable</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["structex",[["impl&lt;'h, H&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"structex/struct.TaggedCaptures.html\" title=\"struct structex::TaggedCaptures\">TaggedCaptures</a>&lt;'h, H&gt;<div class=\"where\">where\n    H: <a class=\"trait\" href=\"structex/re/trait.Sliceable.html\" title=\"trait structex::re::Sliceable\">Sliceable</a> + ?<a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[470]}
+//{"start":57,"fragment_lengths":[617]}
